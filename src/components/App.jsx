3957fa087c49a3d052fa28593e8842eda8f8040b
cwiggin7 <cwiggin7@ksu.edu>
2023-07-22 22:41:33 -0500

    Created Personal Information section in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import "../styles/App.css";
 import AccordionItem from "./AccordionItem";
 import InputForm from "./InputForm";
+import personalData from "../data/personalData";
 import educationData from "../data/educationData";
 import experienceData from "../data/experienceData";
 
@@ -8,6 +9,10 @@ function App() {
   return (
     <>
       <div className="wrapper">
+        <div className="personal-info">
+          <span>Your Personal Info</span>
+          <InputForm inputData={personalData} />
+        </div>
         <div className="accordion">
           <AccordionItem title="Education">
             <InputForm inputData={educationData} />
---
+++ src/data/personalData.js
@@ -0,0 +1,28 @@
+const personalData = [
+  {
+    name: "fullName",
+    label: "Full Name",
+    type: "text",
+    placeholder: "John Smith",
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "text",
+    placeholder: "johnsmith@gmail.com",
+  },
+  {
+    name: "phoneNumber",
+    label: "Phone Number",
+    type: "text",
+    placeholder: "(555) 123-4567",
+  },
+  {
+    name: "location",
+    label: "Location",
+    type: "text",
+    placeholder: "New York, NY",
+  },
+];
+
+export default personalData;
